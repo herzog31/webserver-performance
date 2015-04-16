ff8eb9f0df8942f4879146ef913dd8db9a235ddb
Mark J. Becker <mjb@marbec.com>
2015-04-16 15:25:56 +0200

    Initial commit

---
+++ python/index.py
@@ -0,0 +1,19 @@
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.write("Hello World!")
+        self.finish()
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    server = HTTPServer(application)
+    server.bind(4000)
+    server.start(1)
+    IOLoop.instance().start()
